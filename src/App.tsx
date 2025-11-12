4005a314a01e3a921a844c1eb67193cee8c8eb73
Sam <sram1337@gmail.com>
2025-11-12 13:44:54 -0600

    init repro

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Chessboard } from 'react-chessboard'
+
+function App() {
+  return (
+    <div className="app">
+      <Chessboard boardWidth={800} />
+    </div>
+  )
+}
+
+export default App
